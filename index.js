73782c6e4c22d2280c53f6b7d78b0dbc2aca5a24
froxyzen <froxyzens@gmail.com>
2025-03-31 20:32:50 +0700

    fixed bug

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+document.getElementById("togglePassword").addEventListener("click", function() {
+    let passInput = document.getElementById("pass");
+    if (passInput.type === "password") {
+        passInput.type = "text";
+        this.classList.remove("bi-eye");
+        this.classList.add("bi-eye-slash"); // Ganti ikon ke "mata tertutup"
+    } else {
+        passInput.type = "password";
+        this.classList.remove("bi-eye-slash");
+        this.classList.add("bi-eye"); // Ganti ikon ke "mata terbuka"
+    }
+});
+
 document.addEventListener("DOMContentLoaded", function () {
     const formContainer = document.querySelector('.form-container');
     const registerLink = document.querySelector('.register-link');
